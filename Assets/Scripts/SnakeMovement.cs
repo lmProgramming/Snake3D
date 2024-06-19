21a6da22f624417a71bf78aff096a2b1ce5f3cfd
lmProgramming <mikikubs@gmail.com>
2024-06-19 22:30:12 +0200

    Slight refactoring of snake movement

--- Assets/Scripts/SnakeMovement.cs
+++ Assets/Scripts/SnakeMovement.cs
@@ -11,28 +11,33 @@ public class SnakeMovement : MonoBehaviour
 
     private float activeRoll, activePitch, activeYaw;
 
+    [SerializeField]
+    float boostMultiplier = 4;
 
     private void FixedUpdate()
     {
-        if (snake.alive)
+        if (!snake.alive)
         {
-            float speedMultiplier = 1;
-            if (Input.GetKey(KeyCode.Space))
-            {
-                speedMultiplier = 4;
-            }
-
-            // move forward
-            transform.position += transform.forward * movementSpeed * speedMultiplier * Time.deltaTime;
-
-            activePitch = Input.GetAxisRaw("Vertical") * pitchPower * Time.deltaTime;
-            activeRoll = Input.GetAxisRaw("Rotational") * rollPower * Time.deltaTime;
-            activeYaw = Input.GetAxisRaw("Horizontal") * yawPower * Time.deltaTime;
-
-            transform.Rotate(activePitch * pitchPower * Time.deltaTime,
-                activeYaw * yawPower * Time.deltaTime,
-                -activeRoll * rollPower * Time.deltaTime,
-                Space.Self);
+            return;
         }
+
+        float speedMultiplier = IsBoosting() ? boostMultiplier : 1;
+
+        // move forward
+        transform.position += transform.forward * movementSpeed * speedMultiplier * Time.deltaTime;
+
+        activePitch = Input.GetAxisRaw("Vertical") * pitchPower * Time.deltaTime;
+        activeRoll = Input.GetAxisRaw("Rotational") * rollPower * Time.deltaTime;
+        activeYaw = Input.GetAxisRaw("Horizontal") * yawPower * Time.deltaTime;
+
+        transform.Rotate(activePitch * pitchPower * Time.deltaTime,
+            activeYaw * yawPower * Time.deltaTime,
+            -activeRoll * rollPower * Time.deltaTime,
+            Space.Self);
+    }
+
+    bool IsBoosting()
+    {
+        return Input.GetAxisRaw("Boost") != 0;
     }
 }
